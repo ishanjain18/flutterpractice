a4a9513c8246d921d9eb404fc406ced27b31d595
unknown <ishan10j@gmail.com>
2021-07-01 16:22:14 +0530

    '8ballapp_completed'

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -29,11 +30,21 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset(
-        ('images/ball1.png'),
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          print('$ballNumber');
+        },
+        child: Image.asset(
+          ('images/ball$ballNumber.png'),
+        ),
       ),
     );
   }

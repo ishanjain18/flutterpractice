68833c399ade71e5865abb8e971bdb4c690b3039
unknown <ishan10j@gmail.com>
2021-08-03 18:46:10 +0530

    'FlutterAnimations'

--- flash-chat-flutter-master/lib/main.dart
+++ flash-chat-flutter-master/lib/main.dart
@@ -10,17 +10,13 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          textTheme: TextTheme(
-            body1: TextStyle(color: Colors.black54),
-          ),
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => WelcomeScreen(),
-          '/login': (context) => LoginScreen(),
-          '/register': (context) => RegistrationScreen(),
-          '/chat': (context) => ChatScreen()
-        });
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen()
+      },
+    );
   }
 }
